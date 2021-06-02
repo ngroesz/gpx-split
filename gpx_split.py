22d3b9801a7738ea9a517fb3db9ddf7f083063da
Nick Groesz <nick.groesz@gmail.com>
2021-06-01 20:05:39 -0700

    ok

--- gpx_split.py
+++ gpx_split.py
@@ -1,55 +1,88 @@
+#!/usr/bin/env python3
+
 import argparse
-from bs4 import BeautifulSoup
+import math
+import xml.etree.ElementTree as ET
+import os
+from haversine import haversine, Unit
 
-def gpx_split(xml_file):
-    with open(xml_file, 'r') as fp:
-        xml = fp.read()
+DEFAULT_NAMESPACE = 'http://www.topografix.com/GPX/1/1'
 
-    filter_track_points(xml)
+class GpxFile:
+    def __init__(self, xml_filepath):
+        self.tree = ET.parse(xml_filepath)
+        self.root = self.tree.getroot()
 
-def filter_track_points(xml)
-    track_points = original_track_points(xml)
+        self.namespaces = dict([
+            node for _, node in ET.iterparse(
+                xml_filepath , events=['start-ns']
+            )
+        ])
 
-    print("count: {}".format(len(track_points)))
+        self.xml_file_basename = os.path.basename(xml_filepath)
 
-    track_points = [track_points[0], track_points[1]]
+    def reduce_close_track_points(self, min_distance=10):
+        original_track_points = self.load_track_points()
 
-def original_track_points(xml):
-    tree = BeautifulSoup(xml, 'lxml')
+        reduced_track_points = [original_track_points[0]]
 
-    return tree.find_all('trkpt')
-    #print(tree)
-    #for track_point in tree.find_all('trkpt'):
-    #    print('point: {}'.format(track_point))
-    #
-    #enumerate(track
-    #print("file: {}".format(xml_file))
-    #tree = ET.parse(xml_file)
-    #root = tree.getroot()
+        for track_point in original_track_points:
+            distance = haversine(
+                (float(reduced_track_points[-1].attrib['lat']), float(reduced_track_points[-1].attrib['lon'])),
+                (float(track_point.attrib['lat']), float(track_point.attrib['lon'])),
+                unit=Unit.METERS
+            )
 
-    #print("root: {}".format(root.attrib))
+            if distance >= min_distance:
+                reduced_track_points.append(track_point)
 
-    #my_namespaces = dict([
-    #    node for _, node in ET.iterparse(
-    #        xml_file, events=['start-ns']
-    #    )
-    #])
+        print("reduced to {} points".format(len(reduced_track_points)))
 
-    #print("ns: {}".format(my_namespaces))
+        return reduced_track_points
 
-    ##for track_point in tree.findall('.//{http://www.topografix.com/GPX/1/1}trkpt'):
-    #for track_point in tree.findall('.//{ns}trkpt', namespaces={'ns': 'http://www.topografix.com/GPX/1/1'}):
-    #    print("point: {}".format(track_point))
+    def load_track_points(self):
+        track_points = self.tree.findall('.//{{{}}}trkpt'.format(DEFAULT_NAMESPACE))
+        print("loaded {} points".format(len(track_points)))
+        return track_points
 
+    def reduce_to_max_number_of_points(self, track_points, max_points):
+        if len(track_points) <= max_points:
+            return track_points
+
+        include_ratio = len(track_points) / max_points
+
+        reduced_track_points = []
+        for count, track_point in enumerate(track_points, start=1):
+            if count % include_ratio < 1:
+                reduced_track_points.append(track_point)
+
+        print("reduced to {} points".format(len(reduced_track_points)))
+        return reduced_track_points
+
+    def write_track_points_to_n_files(self, track_points, number_of_files):
+        
+
+    # https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+    def chunks(lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
 def main():
     parser = argparse.ArgumentParser(description="Split GPX tracks")
     parser.add_argument('-p', '--points', default=250, help='Points per file. Defaults to 250')
     parser.add_argument('-f', '--files', default=10, help='Max number of files. Defaults to 10.')
+    parser.add_argument('-d', '--min-distance', type=float, default=10, help='Remove points less than minimum distance. Defaults to 10 meters.')
     parser.add_argument('xml_file')
 
     args = parser.parse_args()
 
-    gpx_split(args.xml_file)
+    print(args)
+    gpx_file = GpxFile(args.xml_file)
 
+    reduced_track_points = gpx_file.reduce_close_track_points(min_distance=args.min_distance)
+    reduced_track_points = gpx_file.reduce_to_max_number_of_points(reduced_track_points, args.points * args.files)
+    gpx_file.write_track_points_to_n_files(track_points, number_of_files=args.files)
+    
+    
 if __name__ == "__main__":
     main()
