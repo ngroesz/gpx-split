92dbf60f8d6fe288d67f77bdca2a45401f176fbd
Nick Groesz <nick.groesz@gmail.com>
2021-06-12 15:02:58 -0700

    checkpoint

--- gpx_split.py
+++ gpx_split.py
@@ -12,72 +12,95 @@ DEFAULT_NAMESPACE = 'http://www.topografix.com/GPX/1/1'
 
 
 class GpxFile:
-    def __init__(self, xml_filepath):
+    def __init__(self, xml_filepath, min_distance, max_points_per_file, output_directory, output_file_count):
         self.tree = etree.parse(xml_filepath)
 
-        self.xml_filename = os.path.basename(xml_filepath)
+        self.xml_filepath        = xml_filepath
+        self.min_distance        = min_distance
+        self.max_points_per_file = max_points_per_file
+        self.output_directory    = output_directory
+        self.output_file_count   = output_file_count
 
-        self.empty_tree = self.tree_without_track_points()
+        self.xml_filename = os.path.basename(self.xml_filepath)
 
-    def tree_without_track_points(self):
-        new_tree = copy.deepcopy(self.tree)
+        self._setup_logger()
 
-        points = new_tree.findall('.//{{{}}}trkpt'.format(DEFAULT_NAMESPACE))
-        for point in points:
-            point.getparent().remove(point)
+    def process_file(self):
+        reduced_trackpoints = self._reduce_close_trackpoints()
 
-        segments = new_tree.findall('.//{{{}}}trkseg'.format(DEFAULT_NAMESPACE))
-        for segment in segments[1:-1]:
-            segment.getparent().remove(segment)
+        max_points = int(self.max_points_per_file) * int(self.output_file_count)
+        reduced_trackpoints = self._reduce_to_max_number_of_points(reduced_trackpoints, max_points)
 
-        return new_tree
+        waypoints = self._translate_trackpoints_to_waypoints(reduced_trackpoints)
 
-    def reduce_close_track_points(self, min_distance=10):
-        original_track_points = self.load_track_points()
+        self._write_waypoints_to_n_files(
+            waypoints,
+            output_directory=self.output_directory,
+            max_number_of_points_per_file=self.max_points_per_file,
+            max_number_of_output_files=self.output_file_count
+        )
 
-        reduced_track_points = [original_track_points[0]]
+    def _waypoint_root(self):
+        root = etree.Element('root',
+            version="1.1",
+            xmlns="http://www.topografix.com/GPX/1/1"
+        )
 
-        for track_point in original_track_points:
+        metadata = etree.SubElement(root, 'metadata')
+        name = etree.Element('name')
+        name.text = 'some name'
+        metadata.append(name)
+
+        return root
+
+    def _reduce_close_trackpoints(self):
+        original_trackpoints = self._load_trackpoints()
+
+        reduced_trackpoints = [original_trackpoints[0]]
+
+        for trackpoint in original_trackpoints:
             distance = haversine(
-                (float(reduced_track_points[-1].attrib['lat']), float(reduced_track_points[-1].attrib['lon'])),
-                (float(track_point.attrib['lat']), float(track_point.attrib['lon'])),
+                (float(reduced_trackpoints[-1].attrib['lat']), float(reduced_trackpoints[-1].attrib['lon'])),
+                (float(trackpoint.attrib['lat']), float(trackpoint.attrib['lon'])),
                 unit=Unit.METERS
             )
 
-            if distance >= min_distance:
-                reduced_track_points.append(track_point)
+            if distance >= self.min_distance:
+                reduced_trackpoints.append(trackpoint)
 
-        print("reduced to {} points".format(len(reduced_track_points)))
+        logging.info("reduced to {} points".format(len(reduced_trackpoints)))
 
-        return reduced_track_points
+        return reduced_trackpoints
 
-    def load_track_points(self):
-        track_points = self.tree.findall('.//{{{}}}trkpt'.format(DEFAULT_NAMESPACE))
-        print("loaded {} points".format(len(track_points)))
-        return track_points
+    def _load_trackpoints(self):
+        trackpoints = self.tree.findall('.//{{{}}}trkpt'.format(DEFAULT_NAMESPACE))
+        logging.info("loaded {} points".format(len(trackpoints)))
+        return trackpoints
 
-    def reduce_to_max_number_of_points(self, track_points, max_points):
-        if len(track_points) <= max_points:
-            return track_points
+    def _reduce_to_max_number_of_points(self, trackpoints, max_points):
+        if len(trackpoints) <= max_points:
+            return trackpoints
 
-        include_ratio = len(track_points) / max_points
+        include_ratio = len(trackpoints) / max_points
 
-        reduced_track_points = []
-        for count, track_point in enumerate(track_points, start=1):
+        reduced_trackpoints = []
+        for count, trackpoint in enumerate(trackpoints, start=1):
             if count % include_ratio < 1:
-                reduced_track_points.append(track_point)
+                reduced_trackpoints.append(trackpoint)
 
-        print("reduced to {} points".format(len(reduced_track_points)))
-        return reduced_track_points
+        logging.info("reduced to {} points".format(len(reduced_trackpoints)))
+        return reduced_trackpoints
 
-    def write_track_points_to_n_files(self, track_points, output_directory, max_number_of_points_per_file, max_number_of_files):
-        if len(track_points) > max_number_of_points_per_file * max_number_of_files:
+    def _write_waypoints_to_n_files(self, waypoints, output_directory, max_number_of_points_per_file, max_number_of_output_files):
+        if len(waypoints) > max_number_of_points_per_file * max_number_of_output_files:
             logging.error(
                 "Directed to write {} points to {} files, with {} points-per-file. This is unpossible"
-                .format(len(track_points), max_number_of_files, max_number_of_points_per_file)
+                .format(len(waypoints), max_number_of_output_files, max_number_of_points_per_file)
             )
 
-        chunked_points = list(self.chunk_list(track_points, max_number_of_points_per_file))
+        waypoint_tree = self._waypoint_root()
+
+        chunked_points = list(self._chunk_list(waypoints, max_number_of_points_per_file))
 
         logging.info("Writing {} files".format(len(chunked_points)))
 
@@ -85,47 +108,50 @@ class GpxFile:
 
         for count, chunk in enumerate(chunked_points, start=1):
             logging.info("Writing {}/{} files".format(count, len(chunked_points)))
-            tree = self.tree_with_new_points(chunk)
 
-            self.write_tree_to_file(
+            output_tree = copy.deepcopy(waypoint_tree)
+
+            for point in chunk:
+                logging.info('point: {}'.format(point))
+                output_tree.append(point)
+
+            self._write_tree_to_file(
                 '{}_-_{}{}'.format(
                     os.path.join(output_directory, file_basename),
                     count,
                     file_extension),
-                tree
+                output_tree
             )
 
-    def tree_with_new_points(self, track_points):
-        new_tree = copy.deepcopy(self.empty_tree)
-        segment = new_tree.find('.//{{{}}}trkseg'.format(DEFAULT_NAMESPACE))
-        for track_point in track_points:
-            segment.append(track_point)
+    def _translate_trackpoints_to_waypoints(self, trackpoints):
+        return list(map(
+            lambda point: etree.Element('wpt', lat=point.attrib['lat'], lon=point.attrib['lon']),
+            trackpoints
+        ))
 
-        return new_tree
 
-    def write_tree_to_file(self, filename, tree):
-        tree.write(filename, pretty_print=True)
+    def _write_tree_to_file(self, filename, tree):
+        root = etree.ElementTree(tree)
+        root.write(filename, pretty_print=True)
 
-    # https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
     @classmethod
-    def chunk_list(cls, array, n):
+    def _chunk_list(cls, array, n):
         for i in range(0, len(array), n):
             yield array[i:i + n]
 
+    def _setup_logger(self):
+        root = logging.getLogger()
+        root.setLevel(logging.DEBUG)
 
-def setup_logger():
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG)
-    root.addHandler(handler)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        root.addHandler(handler)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Split GPX tracks")
     parser.add_argument('-p', '--points', default=250, type=int, help='Points per file. Defaults to 250')
-    parser.add_argument('-f', '--files', default=10, type=int, help='Max number of files. Defaults to 10.')
+    parser.add_argument('-f', '--output-file-count', default=10, type=int, help='Max number of output files. Defaults to 10.')
     parser.add_argument('-d', '--min-distance', type=float, default=10, help='Remove points less than minimum distance. Defaults to 10 meters.')
     parser.add_argument('-o', '--output-directory', help='')
     parser.add_argument('xml_file')
@@ -134,31 +160,22 @@ def parse_args():
 
     if not args.output_directory:
         args.output_directory = os.path.dirname(args.xml_file)
-        print('not {}'.format(args.output_directory))
 
     return args
 
 
-def process_file(args):
-    gpx_file = GpxFile(args.xml_file)
+def main():
+    args = parse_args()
 
-    reduced_track_points = gpx_file.reduce_close_track_points(min_distance=args.min_distance)
-
-    max_points = int(args.points) * int(args.files)
-    reduced_track_points = gpx_file.reduce_to_max_number_of_points(reduced_track_points, max_points)
-
-    gpx_file.write_track_points_to_n_files(
-        reduced_track_points,
+    gpx_file = GpxFile(
+        xml_filepath=args.xml_file,
+        min_distance=args.min_distance,
+        max_points_per_file=args.points,
         output_directory=args.output_directory,
-        max_number_of_points_per_file=args.points,
-        max_number_of_files=args.files
+        output_file_count=args.output_file_count
     )
 
-
-def main():
-    setup_logger()
-    args = parse_args()
-    process_file(args)
+    gpx_file.process_file()
 
 
 if __name__ == "__main__":
